b86a6aa0c5011a13cdf5719fbf6effa51c0a64a0
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-11-15 15:42:18 +0100

    Update inputComponent.js

--- src/components/inputComponent/inputComponent.js
+++ src/components/inputComponent/inputComponent.js
@@ -1,6 +1,4 @@
 import { fetchComponent } from "../../utils/fetchComponent"
-import { printCards } from "../../utils/printCards"
-
 
 export const inputComponent = async () => {
   const header = document.querySelector("header")
@@ -11,18 +9,12 @@ export const inputComponent = async () => {
   `
 
   const input = document.querySelector("#input")
-  let inputValue = ""
 
-  input.addEventListener("change", (e) => {
+  input.addEventListener("change", async (e) => {
     inputValue = e.target.value
+    const images = await fetchComponent(inputValue)
+    console.log(images);
+    let inputValue = ""
   })
-
-  const images = await fetchComponent(inputValue).catch((error) => {
-    console.error("Error fetching images:", error)
-    return []; // Return an empty array if fetch fails
-  })
-
-  printCards(images)
-
 
 }
