b1e7d4d070fafd72f62319ac7755210a84cd85a9
quintero git <hello.quinteroo@gmail.com>
2024-11-17 16:30:28 +0100

    UPDATE cardComponent.css

--- src/components/cardComponent/cardComponent.js
+++ src/components/cardComponent/cardComponent.js
@@ -6,20 +6,28 @@ export const cardComponent = (image) => {
   const card = document.createElement("div");
   card.classList.add("card");
 
+  const imgWrp = document.createElement("div")
+  imgWrp.classList.add("img-wrp")
+
   const imgElement = document.createElement("img");
+  imgElement.classList.add("photo")
   imgElement.src = image.urls.small;
   imgElement.alt = image.alt_description || "Imagen";
   imgElement.title = image.alt_description || "Imagen";
 
   const avatar = document.createElement("img")
+  avatar.classList.add("avatar")
   avatar.src = image.user.profile_image.medium
   avatar.alt = image.user.first_name
   avatar.title = image.user.first_name
 
+  avatar.style.borderColor = getRandomColor();
+
   const user = document.createElement("p");
   user.textContent = image.user.first_name || "Nombre usuario";
 
-  card.appendChild(imgElement);
+  imgWrp.appendChild(imgElement)
+  card.appendChild(imgWrp);
   card.appendChild(avatar)
   card.appendChild(user);
 
